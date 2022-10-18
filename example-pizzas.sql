024620858412445656a7ecbbce89283c5d61d0cf
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-10-18 10:36:10 -0300

    Example-pizzas.sql

---
+++ example-pizzas.sql
@@ -0,0 +1,6 @@
+INSERT INTO pizzas (name, price, ingredients, created_at, updated_at) values
+  ("Margherita", 5, '["tomato", "mozzarella"]', now(), now()),
+  ("Bufala", 6, '["tomato", "mozarella di bufala"]', now(), now()),
+  ("Romana", 5, '["tomato", "mozzarella", "anchovies", "oregano", "oil"]', now(), now()),
+  ("Diavola", 7.5, '["tomato", "mozzarella", "spicy salami"]', now(), now()),
+  ("Pizza Bianca", 5, '["mozzarella", "oregano"]', now(), now());
